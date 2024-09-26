720923c45dd524a872e43758e64a64dbc07686f3
Li-Ruiqi <1009962019@qq.com>
2024-09-26 16:54:43 +0800

    update

--- scripts/train.py
+++ scripts/train.py
@@ -4,7 +4,11 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+    model = YOLO('yolov8n.yaml')
+    model.load('yolov8n.pt') 
 
     # Train the model
-    model.train(data='./data.yaml', epochs=10, imgsz=1024)
+    model.train(cfg='./cfgs/rope_cfg.yaml',
+                data='./datasets/rope_dataset.yaml', 
+                epochs=150, 
+                imgsz=1024)
