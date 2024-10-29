69198b526da5653c1a85f16714c85cfec7441456
Li-Ruiqi <1009962019@qq.com>
2024-10-29 09:28:31 +0800

    update 实验记录

--- scripts/val.py
+++ scripts/val.py
@@ -4,7 +4,7 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 if __name__ == '__main__':
 
-    model = YOLO('/home/jiahan/Desktop/yolo-utils/runs/detect/yolo11n-P2/weights/best.pt')
+    model = YOLO('/home/jiahan/Desktop/yolo-utils/runs/detect/train5/weights/best.pt')
 
     model.val(cfg='./cfgs/rope_cfg.yaml',
                 data='./datasets/rope_dataset.yaml', 
