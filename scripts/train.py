634d1ba3f50fcfab8e668bdb5bdddff9b7842475
Li-Ruiqi <1009962019@qq.com>
2025-01-17 09:17:22 +0800

    1.17：更新船舶分割的实验记录和脚本

--- scripts/train.py
+++ scripts/train.py
@@ -4,12 +4,12 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO('yolo11n-ECA.yaml')
+    model = YOLO('yolo11n-seg.yaml')
     # model.load('/home/jiahan/Desktop/yolo-utils/runs/detect/train4/weights/last.pt')
 
     # Train the model
-    model.train(cfg='./cfgs/rope_cfg.yaml',
-                data='./datasets/rope_dataset.yaml', 
+    model.train(cfg='./cfgs/ship-segment_cfg.yaml',
+                data='./datasets/ship-segment_dataset.yaml', 
                 epochs=350,
                 patience=50,
                 imgsz=1024)
---
+++ ship-segment-实验记录.md
@@ -0,0 +1,11 @@
+# 实验一
+
+- 日期：2025.1.17
+- 模型：原生yolo11n-seg
+- epochs：297
+- 结果：
+
+```
+Class     Images  Instances      Box(P          R      mAP50  mAP50-95)     Mask(P          R      mAP50  mAP50-95): 
+                   all        205        562      0.906      0.877      0.933      0.718      0.904      0.867      0.927      0.667
+```
